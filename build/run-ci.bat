f22b0bc5cd9f3002415c573c31b729918a34bdff
George Ath <GeorgeAth@users.noreply.github.com>
2019-12-15 00:47:32 +0200

    Use new build-scripts with arguments support.

--- build/run-ci.bat
+++ build/run-ci.bat
@@ -9,22 +9,36 @@
 :: Set the default target name.
 if "%AG_TARGET_NAME%"=="" ( SET "AG_TARGET_NAME=AG-DEV" )
 :: Set the default target file.
-if "%AG_TARGET_FILE%"=="" ( SET "AG_TARGET_FILE=.\build\run-ci.bat" )
+if "%AG_TARGET_FILE%"=="" ( SET "AG_TARGET_FILE=.\build\build-script.bat" )
+:: Set the default arguments.
+if "%AG_TARGET_ARGS%"=="" ( SET "AG_TARGET_ARGS= -ci" )
 
-:: LOG: script name.
-echo script  : %0
-:: LOG: project-root.
-echo project : %CD%
-:: Go to parent directory.
-cd ..
-:: LOG: enviroment-root.
-echo root    : %CD%
+:: LOG: (current directory) > (run command) [arguments]
+echo.
+echo %CD%^>%0 %*
 :: Ensure required folder exist and if not exit this script with error 1.
-if not exist %AG_TARGET_NAME% ( echo ERROR: target not found. && exit /b 1 )
+if not exist ../%AG_TARGET_NAME% ( echo ERROR: target not found. && exit /b 1 )
 :: Go to target-root.
-cd %AG_TARGET_NAME%
+echo INFO: Change directory to target-root.
+pushd ..
+pushd %AG_TARGET_NAME%
+echo DIR : %CD%
 :: Run the target.
-CALL %AG_TARGET_FILE%
+SET "AG_RUN_CMD=%AG_TARGET_FILE%%AG_TARGET_ARGS%"
+echo RUN : %AG_RUN_CMD%
+CALL %AG_RUN_CMD%
+echo.
+:: Restore path.
+echo INFO: Retore path.
+popd
+popd
+echo DIR : %CD%
+:: Check for errors.
+IF %ERRORLEVEL% NEQ 0 (
+	echo ERROR: An error occurred while attempting to run: %AG_TARGET_NAME%\%AG_RUN_CMD%
+) else (
+	echo INFO: Run %AG_TARGET_NAME%\%AG_RUN_CMD% complete.
+)
 
 :: Exit this script.
 exit /b
--- build/run-ci.sh
+++ build/run-ci.sh
@@ -3,26 +3,41 @@
 # AG Development Framework.
 # Copyright 2011-2020 GeorgeAth. All Rights Reserved.
 #//////////////////////////////////////////////////////////////////////////////
+# AG-CI : Run.
+#//////////////////////////////////////////////////////////////////////////////
 
 # Set the default target name.
-if [ -z "${AG_TARGET_NAME}" ]; then AG_TARGET_NAME="AG-DEV"; fi
+if [ -z "${AG_TARGET_NAME}" ]; then export AG_TARGET_NAME="AG-DEV"; fi
 # Set the default target file.
-if [ -z "${AG_TARGET_FILE}" ]; then AG_TARGET_FILE="./build/run-ci.sh"; fi
+if [ -z "${AG_TARGET_FILE}" ]; then export AG_TARGET_FILE="./build/build-script.sh"; fi
+# Set the default arguments.
+if [ -z "${AG_TARGET_ARGS}" ]; then export AG_TARGET_ARGS=" -ci"; fi
 
-# LOG: script name.
-echo "script   : $0"
-# LOG: project-root.
-echo "project  : $PWD"
-# Go to parent directory.
-cd ..
-# LOG: enviroment-root.
-echo "env-root : $PWD"
+# LOG: (current directory) > (run command) [arguments]
+echo
+echo "$PWD>$0 $*"
 # Ensure required folders exist and if not exit this script with error 1.
-if [ ! -d "$AG_TARGET_NAME" ]; then { echo "ERROR: target not found."; exit 1; } fi
+if [ ! -d "../$AG_TARGET_NAME" ]; then { echo "ERROR: target not found."; exit 1; } fi
 # Go to target-root.
-cd $AG_TARGET_NAME
+echo "INFO: Change directory to target-root."
+pushd ..
+pushd $AG_TARGET_NAME
+echo "DIR : $PWD"
 # Run the target.
-sh $AG_TARGET_FILE
+AG_RUN_CMD="$AG_TARGET_FILE$AG_TARGET_ARGS"
+sh $AG_RUN_CMD
+echo
+# Restore path.
+echo "INFO: Retore path."
+popd
+popd
+echo "DIR : $PWD"
+# Check for errors.
+if [ $? -ne 0 ]; then {
+	echo "ERROR: An error occurred while attempting to run: $AG_TARGET_NAME/$AG_RUN_CMD";
+} else {
+	echo "INFO: Run $AG_TARGET_NAME/$AG_RUN_CMD complete.";
+} fi
 
 # Exit this script.
-exit 0
+exit $?;
