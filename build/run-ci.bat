4d0aa77ae48d62bcbb2d8d21e860a5972eb2cf1d
George Ath <GeorgeAth@users.noreply.github.com>
2020-01-27 20:20:17 +0200

    Rename variables AG_TARGET_<name> to AG_CI_<name> on run-ci scripts.

--- build/run-ci.bat
+++ build/run-ci.bat
@@ -3,33 +3,35 @@
 :: AG Development Framework.
 :: Copyright 2011-2020 GeorgeAth. All Rights Reserved.
 :://///////////////////////////////////////////////////////////////////////////
-:: Project: AG-CI
-:: Updated: 21-01-2020
+:: Project: AG-CI [master]
+:: Updated: 27-01-2020
 :://///////////////////////////////////////////////////////////////////////////
-:: Description: Run CI.
+:: Description: Run CI/CD.
 :://///////////////////////////////////////////////////////////////////////////
 
-:: Set the default target name.
-if "%AG_TARGET_NAME%"=="" ( SET "AG_TARGET_NAME=AG-DEV" )
+:: Set the default target environment.
+if "%AG_CI_ENV%"=="" ( SET "AG_CI_ENV=.." )
+:: Set the default target project.
+if "%AG_CI_PROJECT%"=="" ( SET "AG_CI_PROJECT=AG-DEV" )
 :: Set the default target file.
-if "%AG_TARGET_FILE%"=="" ( SET "AG_TARGET_FILE=.\scripts\ag.bat" )
+if "%AG_CI_FILE%"=="" ( SET "AG_CI_FILE=.\scripts\ag.bat" )
 :: Set the default arguments.
-if "%AG_TARGET_ARGS%"=="" ( SET "AG_TARGET_ARGS=--ci" )
+if "%AG_CI_ARGS%"=="" ( SET "AG_CI_ARGS=--ci" )
 
 :: LOG: (current directory)>(run command) [arguments]
 echo.
 echo %CD%^>%0 %*
 :: Ensure required folder exist and if not exit this script with error.
-if not exist "..\%AG_TARGET_NAME%" ( echo "ERROR: target not found." && exit /b 1 )
+if not exist "%AG_CI_ENV%\%AG_CI_PROJECT%" ( echo "ERROR: target not found." && exit /b 1 )
 :: Go to target-root.
 echo "INFO: Change directory to target-root."
-pushd "..\%AG_TARGET_NAME%"
+pushd "%AG_CI_ENV%\%AG_CI_PROJECT%"
 echo "DIR : %CD%"
 :: Construct run command.
-if "%AG_TARGET_ARGS%"=="" (
-	SET "AG_RUN_CMD=%AG_TARGET_FILE%
+if "%AG_CI_ARGS%"=="" (
+	SET "AG_RUN_CMD=%AG_CI_FILE%
 ) else (
-	SET "AG_RUN_CMD=%AG_TARGET_FILE% %AG_TARGET_ARGS%"
+	SET "AG_RUN_CMD=%AG_CI_FILE% %AG_CI_ARGS%"
 )
 :: Run the command.
 echo "RUN : %AG_RUN_CMD%"
@@ -42,9 +44,9 @@ popd
 echo "DIR : %CD%"
 :: Check for errors.
 IF %ec% NEQ 0 (
-	echo "ERROR: An error occurred while attempting to run: %AG_TARGET_NAME%\%AG_RUN_CMD%"
+	echo "ERROR: An error occurred while attempting to run: %AG_CI_PROJECT%\%AG_RUN_CMD%"
 ) else (
-	echo "INFO: Run %AG_TARGET_NAME%\%AG_RUN_CMD% complete."
+	echo "INFO: Run %AG_CI_PROJECT%\%AG_RUN_CMD% complete."
 )
 
 :: Exit this script.
--- build/run-ci.sh
+++ build/run-ci.sh
@@ -3,33 +3,35 @@
 # AG Development Framework.
 # Copyright 2011-2020 GeorgeAth. All Rights Reserved.
 #//////////////////////////////////////////////////////////////////////////////
-# Project: AG-CI
-# Updated: 21-01-2020
+# Project: AG-CI [master]
+# Updated: 27-01-2020
 #//////////////////////////////////////////////////////////////////////////////
-# Description: Run CI.
+# Description: Run CI/CD.
 #//////////////////////////////////////////////////////////////////////////////
 
-# Set the default target name.
-if [ -z "${AG_TARGET_NAME}" ]; then export AG_TARGET_NAME="AG-DEV"; fi
+# Set the default target environment.
+if [ -z "${AG_CI_ENV}" ]; then { export AG_CI_ENV=".."; } fi
+# Set the default target project.
+if [ -z "${AG_CI_PROJECT}" ]; then { export AG_CI_PROJECT="AG-DEV"; } fi
 # Set the default target file.
-if [ -z "${AG_TARGET_FILE}" ]; then export AG_TARGET_FILE="./scripts/ag.sh"; fi
+if [ -z "${AG_CI_FILE}" ]; then { export AG_CI_FILE="./scripts/ag.sh"; } fi
 # Set the default arguments.
-if [ -z "${AG_TARGET_ARGS}" ]; then export AG_TARGET_ARGS="--ci"; fi
+if [ -z "${AG_CI_ARGS}" ]; then { export AG_CI_ARGS="--ci"; } fi
 
 # LOG: (current directory)>(run command) [arguments]
 echo
 echo "$PWD>$0 $*"
 # Ensure required folders exist and if not exit this script with error.
-if [ ! -d "../$AG_TARGET_NAME" ]; then { echo "ERROR: target not found."; exit 1; } fi
+if [ ! -d "$AG_CI_ENV/$AG_CI_PROJECT" ]; then { echo "ERROR: target not found."; exit 1; } fi
 # Go to target-root.
 echo "INFO: Change directory to target-root."
-pushd "../$AG_TARGET_NAME" > /dev/null
+pushd "$AG_CI_ENV/$AG_CI_PROJECT" > /dev/null
 echo "DIR : $PWD"
 # Construct run command.
-if [ -z "${AG_TARGET_ARGS}" ]; then {
-	AG_RUN_CMD="$AG_TARGET_FILE";
+if [ -z "${AG_CI_ARGS}" ]; then {
+	AG_RUN_CMD="$AG_CI_FILE";
 } else {
-	AG_RUN_CMD="$AG_TARGET_FILE $AG_TARGET_ARGS";
+	AG_RUN_CMD="$AG_CI_FILE $AG_CI_ARGS";
 } fi
 # Run the command.
 echo "RUN : $AG_RUN_CMD"
@@ -42,9 +44,9 @@ popd > /dev/null
 echo "DIR : $PWD"
 # Check for errors.
 if [ $ec -ne 0 ]; then {
-	echo "ERROR: An error occurred while attempting to run: $AG_TARGET_NAME/$AG_RUN_CMD";
+	echo "ERROR: An error occurred while attempting to run: $AG_CI_PROJECT/$AG_RUN_CMD";
 } else {
-	echo "INFO: Run $AG_TARGET_NAME/$AG_RUN_CMD complete.";
+	echo "INFO: Run $AG_CI_PROJECT/$AG_RUN_CMD complete.";
 } fi
 
 # Exit this script.
